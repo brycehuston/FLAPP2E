a5f088c1550792472fcf5afccd362a89418dc14e
Bryce Huston <bryce.huston@live.ca>
2025-06-29 05:27:53 -0700

    Missing L in FLAPP2E in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            &nbsp;<span className="sm:block hidden">FAPP2E</span>
+            &nbsp;<span className="sm:block hidden">FLAPP2E</span>
           </p>
         </Link>
 
